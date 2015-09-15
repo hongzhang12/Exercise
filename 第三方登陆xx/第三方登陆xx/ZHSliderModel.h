a5f7458d0565f9bd7e82b16ff71d30e431f90685
hongzhang12 <1591320313@qq.com>
2015-09-15 08:41:53 +0800

    cehua

---
+++ 第三方登陆xx/第三方登陆xx/ZHSliderModel.h
@@ -0,0 +1,13 @@
+//
+//  ZHSliderModel.h
+//  第三方登陆xx
+//
+//  Created by zhanghong on 15/9/15.
+//  Copyright (c) 2015年 zhanghong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZHSliderModel : NSObject
+
+@end
---
+++ 第三方登陆xx/第三方登陆xx/ZHSliderModel.m
@@ -0,0 +1,13 @@
+//
+//  ZHSliderModel.m
+//  第三方登陆xx
+//
+//  Created by zhanghong on 15/9/15.
+//  Copyright (c) 2015年 zhanghong. All rights reserved.
+//
+
+#import "ZHSliderModel.h"
+
+@implementation ZHSliderModel
+
+@end
